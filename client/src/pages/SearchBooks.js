3f26f1b141875eec88a339f57c8f7c8c87027ae3
Lantao home PC <lantaook@gmail.com>
2023-03-08 00:03:48 -0500

    started front end, queries.js, mutations.js, app.js, searchbooks.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import {SAVE_BOOK} from '../utils/mutations'
 import {
   Container,
   Col,
@@ -26,6 +28,8 @@ const SearchBooks = () => {
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
+
+  const [savebook_mut, { error }] = useMutation(SAVE_BOOK);
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -72,11 +76,17 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      // const response = await saveBook(bookToSave, token);
+      const { data } = await savebook_mut({
+        variables: {
+          id: Auth.getProfile().data._id,
+          input: bookToSave
+        }
+      })
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,43 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation Mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation Mutation($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation Mutation($saveBookId: ID!, $input: BookInput) {
+    saveBook(id: $saveBookId, input: $input) {
+      username
+      _id
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation Mutation($bookId: String) {
+    removeBook(bookId: $bookId) {
+      username
+      _id
+    }
+  }
+`;
